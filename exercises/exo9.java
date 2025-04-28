22ff5a56c3bea2aec43462bc490663c6333f6d7c
weiss67 <weissj.pro@gmail.com>
2025-04-28 09:24:21 +0200

    Message

--- exercises/exo9.java
+++ exercises/exo9.java
@@ -38,3 +38,49 @@
  * 
  * 
 */
+
+import java.util.Scanner;
+
+public class exo9 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+ 
+        System.out.println("Votre prénom ?");
+        //Boolean a = sc.nextBoolean();
+        String firstname = sc.nextLine();
+
+        System.out.println("Votre nom ?");
+        String lastname = sc.nextLine();
+        
+        System.out.println("Votre adresse mail ?");
+        String mail = sc.nextLine();
+
+        System.out.println("Votre mot de passe ?");
+        String createmdp = sc.nextLine();
+
+        System.out.println("Confirmer votre mot de passe ?");
+        String checkmdp = sc.nextLine();
+
+        //if (a.equals("y") || a.equals("y")) { //fonctionnel
+        if (checkmdp.equals(createmdp)) {
+        System.out.println("Merci "+ firstname +" "+ lastname +" , votre inscription a bien été effectuée, vous recevrez un mail de confirmation à l'adresse : "+ mail);
+        } else { 
+            System.out.println("Inscription annulée");
+        }
+
+        System.out.println("Pour vous connecter, renseignez votre email ?");
+        String checkmail = sc.nextLine();
+
+        System.out.println("Renseignez votre mot de passe ?");
+        String checkmdp2 = sc.nextLine();
+
+        if (checkmdp2.equals(checkmdp) && checkmail.equals(mail)) {
+            System.out.println("Connexion réussie");
+            } else { 
+                System.out.println("Connexion refusée");
+            }
+
+        sc.close();
+    }
+}
